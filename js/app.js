4cf471188bcdf7b7fd2ec67210331f32c2956d63
arparvej1 <parvej8pro@gmail.com>
2024-02-13 20:46:09 +0600

    add area calculation remove function

--- js/app.js
+++ js/app.js
@@ -1,22 +1,10 @@
-const areaCalculation = document.getElementById('area-calculation');
-
-areaCalculation.innerText = "hello";
 
 function triangleCalculator() {
     const triangleBase = inputFieldToValue("triangle-base");
-    console.log(triangleBase);
     const triangleHight = inputFieldToValue("triangle-hight");
-    console.log(triangleHight);
-    const area = 0.5* triangleBase * triangleHight;
-    console.log(area);
-    areaPrint(area, 'triangle-area');
-    // const print = document.getElementById('triangle-area');
-    // print.innerText = area;
+    const area = 0.5 * triangleBase * triangleHight;
+    areaPrint(area, 'triangle-area', 'Triangle'); // call print
 }
-
-// const btnTriangle = document.getElementById('btn-triangle');
-// console.log(btnTriangle);
-// btnTriangle.addEventListener('click', console.log(triangleCalculator()));
 
 function inputFieldToValue(fieldId) {
     const inputField = document.getElementById(fieldId);
@@ -25,7 +13,37 @@ function inputFieldToValue(fieldId) {
     return inputNum;
 }
 
-function areaPrint(area, id){
+function areaPrint(area, id, shape) {
     const print = document.getElementById(id);
     print.innerText = area;
+    const li = document.createElement('li');
+    li.innerHTML = `<span>${shape}: ${area} cm<sup>2</sup></span><span class="item-x"> x </span>`;
+    li.classList.add('flex');
+    li.classList.add('justify-between');
+    const items = document.getElementById('area-calculation-list');
+    items.appendChild(li);
 }
+
+// remove list
+// function removeItems() {
+//     const itemsX = document.getElementsByClassName('item-x');
+//     for (const item of itemsX) {
+//         item.addEventListener('click', function (event) {
+//             event.target.parentNode.remove();
+//         })
+//     }
+// }
+
+// document.getElementById('area-calculation-list').addEventListener('click', function () {
+//     removeItems();
+// });
+
+
+document.getElementById('area-calculation-list').addEventListener('click', function () {
+    const items = document.getElementsByClassName('item-x');
+    for (const item of items) {
+        item.addEventListener('click', function (event) {
+            event.target.parentNode.remove();
+        })
+    }
+});
