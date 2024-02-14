7d8e1b6c2dd9880429f2e8e252ea43ab2ee41a7c
arparvej1 <parvej8pro@gmail.com>
2024-02-14 16:47:17 +0600

    ready 6 card

--- js/app.js
+++ js/app.js
@@ -2,33 +2,44 @@
 function triangleCalculator() {
     const triangleBase = inputFieldToValue("triangle-base");
     const triangleHight = inputFieldToValue("triangle-hight");
-    const area = 0.5 * triangleBase * triangleHight;
+    let area = 0.5 * triangleBase * triangleHight;
     areaPrint(area, 'triangle-area', 'Triangle');
-    console.log(triangleBase, triangleHight);
 }
 
 function rectangleCalculator() {
     const rectangleW = inputFieldToValue("rectangle-w");
     const rectangleL = inputFieldToValue("rectangle-l");
-    const area = rectangleW * rectangleL;
+    let area = rectangleW * rectangleL;
     areaPrint(area, 'rectangle-area', 'Rectangle');
-    console.log(rectangleW, rectangleL);
 }
 
 function parallelogramCalculator() {
     const parallelogramB = inputFieldToValue("parallelogram-b");
     const parallelogramH = inputFieldToValue("parallelogram-h");
-    const area = parallelogramB * parallelogramH;
+    let area = parallelogramB * parallelogramH;
     areaPrint(area, 'parallelogram-area', 'Parallelogram');
-    console.log(parallelogramB, parallelogramH);
 }
 
 function rhombusCalculator() {
     const rhombusD1 = inputFieldToValue("rhombus-d1");
     const rhombusD2 = inputFieldToValue("rhombus-d2");
-    const area = 0.5 * rhombusD1 * rhombusD2;
+    let area = 0.5 * rhombusD1 * rhombusD2;
     areaPrint(area, 'rhombus-area', 'Rhombus');
-    console.log(rhombusD1, rhombusD2);
+}
+
+function pentagonCalculator() {
+    const pentagonP = inputFieldToValue("pentagon-p");
+    const pentagonB = inputFieldToValue("pentagon-b");
+    let area = 0.5 * pentagonP * pentagonB;
+    areaPrint(area, 'pentagon-area', 'Pentagon');
+}
+
+function ellipseCalculator() {
+    const ellipseA = inputFieldToValue("ellipse-a");
+    const ellipseB = inputFieldToValue("ellipse-b");
+    const pi = 3.141592653589793238462643383279502884197;
+    let area = pi * ellipseA * ellipseB;
+    areaPrint(area, 'ellipse-area', 'Ellipse');
 }
 
 // common for all card
@@ -41,6 +52,7 @@ function inputFieldToValue(fieldId) {
 
 function areaPrint(area, id, shape) {
     const print = document.getElementById(id);
+    area !== Math.floor(area) ? area = area.toFixed(2) : null;
     print.innerText = area;
     const li = document.createElement('li');
     li.innerHTML = `<span>${shape}: ${area} cm<sup>2</sup></span><span class="item-x">&#x2718;</span>`;
